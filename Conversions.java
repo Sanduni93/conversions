912f07da7f0397ffa4cf92dd2145308a00af86ea
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-07-17 16:36:42 +0530

    Explicit and Implicit conversions in java

---
+++ Conversions.java
@@ -0,0 +1,22 @@
+class Conversions{
+	
+	public static void main(String args[]){
+	
+		double a = 3.12532;
+		int b = (int) a;
+		System.out.println("Example for Explicit conversion");
+	    System.out.println("a:"+a);
+		System.out.println("b:"+b);
+		
+		int c = 12;
+		double d = c;
+		System.out.println("Example for Implicit conversion");
+		System.out.println("c:"+c);
+		System.out.println("d:"+d);
+		
+		
+		
+		
+	}
+	
+}
